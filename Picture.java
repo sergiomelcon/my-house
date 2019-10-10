89ba2fcd57f331b576c237cc0787c8f866aafaa7
sergiomelcon <sermelgio@gmail.com>
2019-10-10 13:50:11 +0200

    atardecer

--- Picture.java
+++ Picture.java
@@ -93,4 +93,14 @@ public class Picture
             sun.changeColor("yellow");
         }
     }
+    /**
+     * Sol bajando como el atardecer
+     */
+    public void atardecer()
+    {
+        if (wall != null)   // only if it's painted already...
+        {
+            sun.slowMoveVertical(50);
+        }
+    }
 }
